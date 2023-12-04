a1ae3bb35a713f6533359c675c00d751c43e2371
valentinMachado <valentin.machado.cpe@gmail.com>
2023-12-04 15:44:09 +0100

    npm run create-token-secret do not overwrite all the .env

--- bin/createTokenSecret.js
+++ bin/createTokenSecret.js
@@ -1,6 +1,27 @@
-const THREE = require('three');
 const fs = require('fs');
+const { MathUtils } = require('three');
 
-const newSecret = THREE.MathUtils.generateUUID();
+const data = fs.readFileSync('./.env', { encoding: 'utf-8' });
 
-fs.writeFileSync('./.env', 'TOKEN_SECRET=' + newSecret);
+// split the contents by new line
+const lines = data.split(/\r?\n/);
+
+// remove lines starting with TOKEN_SECRET
+for (let index = lines.length - 1; index >= 0; index--) {
+  const line = lines[index];
+  if (line.startsWith('TOKEN_SECRET') || line == '') {
+    lines.splice(index, 1);
+  }
+}
+
+// new secret
+lines.push('TOKEN_SECRET=' + MathUtils.generateUUID());
+
+fs.writeFileSync('./.env', '');
+
+// mermaid graph generation
+lines.forEach((line) => {
+  fs.appendFileSync('./.env', '\n' + line, (err) => {
+    if (err) throw err;
+  });
+});
